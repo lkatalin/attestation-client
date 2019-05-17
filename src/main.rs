9c2b5f8f2ba60c46cf18eab3f47f628281f4b5e6
Lily S <lsturman@redhat.com>
2019-05-17 16:41:48 -0400

    makes attestation request

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,28 @@
-use sgx_isa::{Report, Targetinfo, Keyrequest, Keyname};
+//use sgx_isa::{Report, Targetinfo, Keyrequest, Keyname};
 use std::net::{TcpStream};
-use std::io::{self, Read, Write};
-use crypto_mac::Mac;
-use cmac::Cmac;
-use aes::Aes128;
+use std::io::{self, Write};
 
-// verify that attesting enclave is on same CPU as this enclave
-// based on 
-// https://gist.github.com/Vinc0682/10c074202c995e4f87b4edf278ec4cae
-fn verify_report(report: &Report) -> bool {
+//use dcap_ql::quote;
+//use dcap_ql::{target_info};
 
-    // create a key request containing keyname and keyid
-    let key_request = Keyrequest {
-        keyname: Keyname::Report as _,
-        keyid: report.keyid.clone(),
-        ..Default::default()
-    };
-
-    // call EGETKEY with key_request parameters to get report's key
-    let key = key_request.egetkey().expect("failed to get key");
-    
-    // extract the MAC from the report
-    let report_ref: &[u8] = report.as_ref();
-    let mut mac_from_report = &report_ref[0..Report::UNPADDED_SIZE-48];
-
-    // compute the MAC for the EGETKEY result
-    let mut mac_computed = Cmac::<Aes128>::new_varkey(&key[..]).unwrap();
-    mac_computed.input(&mut mac_from_report);
-
-    // verify that computed MAC matches MAC from report
-    mac_computed.verify(&report.mac).is_ok()
+// request attestation from daemon
+fn request_attestation() -> Result<(), io::Error> {
+    match TcpStream::connect("localhost:1034") {
+        Ok(mut stream) => {
+            let req = b"Request attestation";
+            match stream.write(req) {
+                Ok(_) => (),
+                Err(e) => return Err(e),
+            };
+        },
+        Err(_) => {
+            panic!("Client unable to connect to daemon.");
+        }
+    }
+    Ok(())
 }
 
 fn main() -> io::Result<()> {
-
-    // find targetinfo for current enclave
-    let targetinfo = Targetinfo::from(Report::for_self());
-    
-    // connect to server or 'attesting' enclave
-    match TcpStream::connect("localhost:1035") {
-        Ok(mut stream) => {
-
-            println!("\nSuccessfully connected to server on port 1035.\n");
-
-            // send current enclave's targetinfo to attesting enclave
-            stream.write(targetinfo.as_ref()).unwrap();
-            
-            // read report that attesting enclave created for 
-            // target info that was sent
-            let mut data = [0; Report::UNPADDED_SIZE];
-            match stream.read_exact(&mut data) {
-                Ok(_) => {
-                    let rep_back = Report::try_copy_from(&data).unwrap();
-
-                    // calculate MAC for report key and verify that
-                    // it matches MAC in report
-                    let does_match = verify_report(&rep_back);
-
-                    println!("MAC matches: {}\n", does_match);
-                },
-
-                Err(e) => {
-                    println!("Failed to receive data: {}", e);
-                }
-            }
-        },
-        Err(e) => {
-            println!("Failed to connect: {}", e);
-        }
-        
-    }
+    request_attestation();
     Ok(())
 }
